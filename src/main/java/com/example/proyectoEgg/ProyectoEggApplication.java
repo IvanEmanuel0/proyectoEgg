3628c022cb03c5017d943b64aa62ac87b9c42a24
Javier Loria <javieralejandroloria@gmail.com>
2021-11-29 21:11:21 -0300

    segunda clase

--- src/main/java/com/example/proyectoEgg/ProyectoEggApplication.java
+++ src/main/java/com/example/proyectoEgg/ProyectoEggApplication.java
@@ -10,4 +10,6 @@ public class ProyectoEggApplication {
 		SpringApplication.run(ProyectoEggApplication.class, args);
 	}
 
+
+
 }
---
+++ src/main/java/com/example/proyectoEgg/entity/Gasto.java
@@ -0,0 +1,4 @@
+package com.example.proyectoEgg.entity;
+
+public class Gasto {
+}
