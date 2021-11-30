4c1522a385998e26544d8339b3847e604bcece33
Javier Loria <javieralejandroloria@gmail.com>
2021-11-30 20:43:23 -0300

    Cuenta y Rol

--- src/main/java/com/example/proyectoEgg/entity/Rol.java
+++ src/main/java/com/example/proyectoEgg/entity/Rol.java
@@ -1,8 +1,13 @@
 package com.example.proyectoEgg.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 
 @Entity
+@Getter
+@Setter
 public class Rol {
 
     Integer id;
