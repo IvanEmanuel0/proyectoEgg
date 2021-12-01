2724eafb94c6ee106b6cab7751cc4b1e94978aa7
demianduarte7 <nicolasdemianduarte@gmail.com>
2021-11-30 21:47:25 -0300

    entidad demi:$

--- src/main/java/com/example/proyectoEgg/entity/Deuda.java
+++ src/main/java/com/example/proyectoEgg/entity/Deuda.java
@@ -3,5 +3,37 @@ package com.example.proyectoEgg.entity;
 public class Deuda {
 
     //Lo va a escribir demi
+    Integer id;
+    Double montoAPagar;
+    Boolean alta;
 
+    public Deuda(Integer id, Double montoAPagar, Boolean alta) {
+        this.id = id;
+        this.montoAPagar = montoAPagar;
+        this.alta = alta;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getMontoAPagar() {
+        return montoAPagar;
+    }
+
+    public void setMontoAPagar(Double montoAPagar) {
+        this.montoAPagar = montoAPagar;
+    }
+
+    public Boolean getAlta() {
+        return alta;
+    }
+
+    public void setAlta(Boolean alta) {
+        this.alta = alta;
+    }
 }
