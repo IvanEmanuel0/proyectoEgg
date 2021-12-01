17ac76955dd2a06968034775d48f081ec063faa3
Javier Loria <javieralejandroloria@gmail.com>
2021-11-30 21:01:08 -0300

    Cuenta y Rol

--- src/main/java/com/example/proyectoEgg/entity/Cuenta.java
+++ src/main/java/com/example/proyectoEgg/entity/Cuenta.java
@@ -4,14 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import java.time.LocalDate;
 
 @Entity
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Cuenta {
 
     Integer id;
@@ -35,4 +38,5 @@ public class Cuenta {
         this.fechaDeModificacion = fechaDeModificacion;
         this.alta = alta;
     }
+
 }
