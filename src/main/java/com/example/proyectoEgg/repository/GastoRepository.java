d8ef722c4b904ccc2acd3e4201f357613b94dc2c
Fer <fernanda.fano@gmail.com>
2021-11-30 21:34:26 -0300

    Cuarto commit (Fer).

--- src/main/java/com/example/proyectoEgg/repository/GastoRepository.java
+++ src/main/java/com/example/proyectoEgg/repository/GastoRepository.java
@@ -1,11 +1,6 @@
 package com.example.proyectoEgg.repository;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.stereotype.Service;
-
-import javax.persistence.Column;
-import java.time.LocalDate;
 
 @Service
 public interface GastoRepository {
