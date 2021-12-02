483fdf8f5fffd00a030a944aacf12867442c67be
Iván Alagastino <alagastino.ivane@gmail.com>
2021-12-02 20:32:25 -0300

    clase exception

---
+++ src/main/java/com/example/proyectoEgg/exception/MiException.java
@@ -0,0 +1,11 @@
+package com.example.proyectoEgg.exception;
+
+public class MiException extends Exception {
+
+    public MiException() {
+    }
+
+    public MiException(String msg) {
+        super(msg);
+    }
+}
